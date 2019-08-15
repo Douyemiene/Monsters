bd0a60b50fc9dbdbcf3763d0119294ca7a93a417
INSPIRON <viktormiene.vm@gmail.com>
2019-08-15 06:05:38 -0700

    Updates

---
+++ precache-manifest.8869cc6671100ba143cb59cb40684a5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a78c3354446b57096503dd0362e29b8",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "96a1b0d86f73bceebd1f",
+    "url": "/Monsters/static/css/main.ea0ec2a8.chunk.css"
+  },
+  {
+    "revision": "90bc354edad42df017e0",
+    "url": "/Monsters/static/js/2.08bb4953.chunk.js"
+  },
+  {
+    "revision": "96a1b0d86f73bceebd1f",
+    "url": "/Monsters/static/js/main.0a0901b5.chunk.js"
+  },
+  {
+    "revision": "88d7a8d8b4d4be6c6685",
+    "url": "/Monsters/static/js/runtime~main.10c642f3.js"
+  }
+]);
